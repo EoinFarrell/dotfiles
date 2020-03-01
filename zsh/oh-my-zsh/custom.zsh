c967d04f8a6875f342f711db96f787a255611085
EoinFarrell <efarrell@zendesk.com>
2020-03-01 18:48:52 +0000

    Moving zsh config to custom file so its in 1 place

--- zsh/oh-my-zsh/custom.zsh
+++ zsh/oh-my-zsh/custom.zsh
@@ -1,3 +1,53 @@
+export TERM="xterm-256color"
+
+THEMESD=$HOME/.oh-my-zsh/custom/themes
+DIRECTORY=$THEMESD/powerlevel9k
+if [ ! -d "$DIRECTORY" ]; then
+    git clone https://github.com/bhilburn/powerlevel9k.git $DIRECTORY
+fi
+
+ZSH_THEME="powerlevel9k/powerlevel9k"
+
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(dir rbenv vcs)
+POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status root_indicator background_jobs history time)
+POWERLEVEL9K_PROMPT_ON_NEWLINE=true
+POWERLEVEL9K_PROMPT_ADD_NEWLINE=true
+POWERLEVEL9K_VCS_MODIFIED_BACKGROUND='red'
+
+# Add a space in the first prompt   
+POWERLEVEL9K_MULTILINE_FIRST_PROMPT_PREFIX="%f"
+# Visual customisation of the second prompt line
+local user_symbol="$"
+if [[ $(print -P "%#") =~ "#" ]]; then
+    user_symbol = "#"
+fi
+POWERLEVEL9K_MULTILINE_LAST_PROMPT_PREFIX="%{%B%F{black}%K{yellow}%} $user_symbol%{%b%f%k%F{yellow}%} %{%f%}"
+
+PLUGINSD=$ZSH_CUSTOM/plugins
+DIRECTORY=$PLUGINSD/zsh-autosuggestions
+if [ ! -d "$DIRECTORY" ]; then
+    git clone https://github.com/zsh-users/zsh-autosuggestions $DIRECTORY
+fi
+
+# Which plugins would you like to load?
+# Standard plugins can be found in ~/.oh-my-zsh/plugins/*
+# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
+# Example format: plugins=(rails git textmate ruby lighthouse)
+# Add wisely, as too many plugins slow down shell startup.
+plugins=(
+    colored-man-pages
+    docker
+    docker-compose
+    git
+    kubectl
+    osx
+    zsh-autosuggestions
+)
+
+source $ZSH/oh-my-zsh.sh
+
+source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
 DOTFILES=~/Code/dotfiles
 alias weather="curl http://v2.wttr.in"
 
