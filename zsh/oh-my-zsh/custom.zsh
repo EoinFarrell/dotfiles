993d3e706991f561210344ff1bd12cea7c9b6f71
EoinFarrell <efarrell@zendesk.com>
2020-03-14 15:44:43 +0000

    Moving theming all into custom.zsh

--- zsh/oh-my-zsh/custom.zsh
+++ zsh/oh-my-zsh/custom.zsh
@@ -38,4 +38,34 @@ echo -e "\033]6;1;bg;red;brightness;18\a"
 echo -e "\033]6;1;bg;green;brightness;26\a"
 echo -e "\033]6;1;bg;blue;brightness;33\a"
 
+export TERM="xterm-256color"
+
+THEMESD=$HOME/.oh-my-zsh/custom/themes
+DIRECTORY=$THEMESD/powerlevel9k
+if [ ! -d "$DIRECTORY" ]; then
+    git clone https://github.com/bhilburn/powerlevel9k.git $DIRECTORY
+fi
+
+# Set name of the theme to load --- if set to "random", it will
+# load a random theme each time oh-my-zsh is loaded, in which case,
+# to know which specific one was loaded, run: echo $RANDOM_THEME
+# See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
+# ZSH_THEME="robbyrussell"
+ZSH_THEME="powerlevel9k/powerlevel9k"
+
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(dir rbenv vcs)
+POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status root_indicator background_jobs history time)
+POWERLEVEL9K_PROMPT_ON_NEWLINE=true
+POWERLEVEL9K_PROMPT_ADD_NEWLINE=true
+POWERLEVEL9K_VCS_MODIFIED_BACKGROUND='red'
+
+# Add a space in the first prompt
+POWERLEVEL9K_MULTILINE_FIRST_PROMPT_PREFIX="%f"
+# Visual customisation of the second prompt line
+local user_symbol="$"
+if [[ $(print -P "%#") =~ "#" ]]; then
+    user_symbol = "#"
+fi
+POWERLEVEL9K_MULTILINE_LAST_PROMPT_PREFIX="%{%B%F{black}%K{yellow}%} $user_symbol%{%b%f%k%F{yellow}%} %{%f%}"
+
 eval "$(rbenv init -)"
