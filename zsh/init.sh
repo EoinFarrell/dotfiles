40bc4f980d243bfe5549cb49675adbf001aa3a32
Eoin Farrell <me@eoinfarrell.dev>
2023-02-17 12:19:54 +0000

    Docker alias was running, slowing startup

--- zsh/init.sh
+++ zsh/init.sh
@@ -2,26 +2,30 @@ DOTFILES=~/Code/personal/dotfiles
 
 source $DOTFILES/zsh/functions.sh
 
-func_result="$(isTmuxSession)"
+getLatestPackages() {
+  func_result="$(isTmuxSession)"
 
-if [ $func_result  -eq 0 ]; then
-  brew outdated & disown
+  if [ $func_result  -eq 0 ]; then
+    brew outdated
 
-  PLUGINSD=$ZSH_CUSTOM/plugins
+    PLUGINSD=$ZSH_CUSTOM/plugins
 
-  DIRECTORY=$PLUGINSD/zsh-autosuggestions
-  getLatestFromGit $DIRECTORY "https://github.com/zsh-users/zsh-autosuggestions" & disown
+    DIRECTORY=$PLUGINSD/zsh-autosuggestions
+    getLatestFromGit $DIRECTORY "https://github.com/zsh-users/zsh-autosuggestions"
 
-  DIRECTORY=$PLUGINSD/zsh-syntax-highlighting
-  getLatestFromGit $DIRECTORY "https://github.com/zsh-users/zsh-syntax-highlighting" & disown
+    DIRECTORY=$PLUGINSD/zsh-syntax-highlighting
+    getLatestFromGit $DIRECTORY "https://github.com/zsh-users/zsh-syntax-highlighting"
 
-  DIRECTORY=$PLUGINSD/zsh-autosuggestions
-  getLatestFromGit $DIRECTORY "https://github.com/zsh-users/zsh-completions" & disown
-  fpath+=$DIRECTORY/src
+    DIRECTORY=$PLUGINSD/zsh-autosuggestions
+    getLatestFromGit $DIRECTORY "https://github.com/zsh-users/zsh-completions"
+    fpath+=$DIRECTORY/src
 
-  THEMESD=$HOME/.oh-my-zsh/custom/themes
-  DIRECTORY=$THEMESD/powerlevel10k
-  getLatestFromGit $DIRECTORY "https://github.com/romkatv/powerlevel10k.git" & disown
-else
-  echo 'Tmux session - Latest from Github not pulled'
-fi
+    THEMESD=$HOME/.oh-my-zsh/custom/themes
+    DIRECTORY=$THEMESD/powerlevel10k
+    getLatestFromGit $DIRECTORY "https://github.com/romkatv/powerlevel10k.git"
+  else
+    echo 'Tmux session - Latest from Github not pulled'
+  fi
+}
+
+getLatestPackages
